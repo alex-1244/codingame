7c23b7d9188de793822cead8c7b41f3fa57bc976
Alex Tertyshnyk <alex@tertyshnyk.com>
2017-02-25 15:27:48 +0200

    ChuckNorris soved

---
+++ ChuckNorris/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution
+{
+    static void Main(string[] args)
+    {
+        string MESSAGE = Console.ReadLine();
+
+        byte[] bytes = Encoding.ASCII.GetBytes(MESSAGE);
+        var S = string.Join("", bytes.Select(b =>
+        {
+            var s = Convert.ToString(b, 2);
+            var missingZeros = 7 - s.Length;
+            
+            s = (missingZeros==0)? s : (new int[missingZeros]).Select(x => "0").Aggregate((x, y) => x + y) + s;
+            return s;
+        }));
+
+        var res = "";
+        char cPrev = '2';
+        foreach (var c in S)
+        {
+            if (c == '0' && c != cPrev)
+                res += " 00 0";
+            else if (c == '0' && c == cPrev)
+                res += "0";
+            else if (c == '1' && c != cPrev)
+                res += " 0 0";
+            else if (c == '1' && c == cPrev)
+                res += "0";
+            cPrev = c;
+        }
+
+        Console.WriteLine(res.Trim());
+    }
+}
