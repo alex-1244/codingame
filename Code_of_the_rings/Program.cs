1e03a2e72bc0e6d8e0b05926c5ff51e89b6ceb26
AlexTertyshnyk <alex112244@gmail.com>
2019-01-07 15:39:19 +0200

    code of the rings initialized skynet-2 clen-up

---
+++ Code_of_the_rings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Code_of_the_rings
+{
+	//https://www.codingame.com/ide/puzzle/code-of-the-rings
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string magicPhrase = Console.ReadLine();
+
+			// Write an action using Console.WriteLine()
+			// To debug: Console.Error.WriteLine("Debug messages...");
+
+			Console.WriteLine("+.>-.");
+		}
+	}
+}
