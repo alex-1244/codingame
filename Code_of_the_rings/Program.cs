28333f3b6e88397b0efdd1dd3f9fa1047811b366
AlexTertyshnyk <alex112244@gmail.com>
2019-01-08 00:05:22 +0200

    movemnt algorithm first implementation

--- Code_of_the_rings/Program.cs
+++ Code_of_the_rings/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-using System.Net.Sockets;
-using System.Runtime.InteropServices;
 
 namespace Code_of_the_rings
 {
@@ -33,32 +32,43 @@ namespace Code_of_the_rings
 
 		private readonly BoardState _state;
 		private readonly int _playerPosition;
-		private readonly int[] _remainingLetters;
+		private int[] _remainingLetters;
+		private int[] _parsedLetters;
 
-		public Strategy(BoardState initialState, int[] remainingLetters, int playerPosition)
+		public Strategy(BoardState initialState, int[] remainingLetters, int[] parsedLetters, int playerPosition)
 		{
-
 			this._state = initialState;
 			this._remainingLetters = remainingLetters;
+			this._parsedLetters = parsedLetters;
 			this._playerPosition = playerPosition;
-			this.Hash = this.GetHashCode();
+			this.Hash = this.GetStrategyString();
 		}
 
+		//жадный алгоритм, на каждом шаге искать самое оптимальное решение
 		public void MakeMove()
 		{
-			var moveFromCurrentPosition = _state.Transform(_playerPosition, _remainingLetters[0]);
-			var moveFromLeftPosition = _state.Transform(_playerPosition - 1, _remainingLetters[0]);
-			var moveFromRightPosition = _state.Transform(_playerPosition + 1, _remainingLetters[0]);
+			if (_remainingLetters.Length == 0)
+			{
+				throw new InvalidOperationException("Solution found, make something!!");
+			}
 
-			need to change classes, Idea:
-				- store movement, _remainingLetters letters and player position together,
-				- then recuirsivelly make changes to the states, store AllowReversePInvokeCallsAttribute the descision tree somewhere,
-					compare for dublicates and delete them.
-				- if number of descisions > 1000 (for example)
-					Select the best descision (compare by average number of moves for the letter (Moves.Length/numberOfPrintedLetters))
+			var allPosibleStates = new List<BoardState>();
+			var nextLetter = _remainingLetters[0];
+
+			allPosibleStates = _state.State.Select((x, i) => _state.Transform(i, x)).ToList();
+
+			var bestState = allPosibleStates.GroupBy(x => x.GetDistanceTo(_state)).OrderBy(x => x.Key).First().First();
+
+			var movement = _state.MoveTo(bestState);
+			//need to change classes, Idea:
+			//	- store movement, _remainingLetters letters and player position together,
+			//	- then recuirsivelly make changes to the states, store AllowReversePInvokeCallsAttribute the descision tree somewhere,
+			//		compare for dublicates and delete them.
+			//	- if number of descisions > 1000 (for example)
+			//		Select the best descision (compare by average number of moves for the letter (Moves.Length/numberOfPrintedLetters))
 		}
 
-		private string GetHashCode()
+		private string GetStrategyString()
 		{
 			return AlphabetConverter.ToWord(_state.State)
 				   + _playerPosition
@@ -68,13 +78,16 @@ namespace Code_of_the_rings
 
 	public class BoardState
 	{
-		public int[] State { get; }
-		public string Move { get; private set; }
+		public const int BoardSize = 30;
 
-		public BoardState(int[] state)
+		public BoardState(int[] state, int playerPosition)
 		{
 			State = state;
+			PlayerPosition = playerPosition;
 		}
+
+		public int[] State { get; }
+		public int PlayerPosition { get; private set; }
 
 		public BoardState Transform(int position, int desiredState)
 		{
@@ -90,14 +103,53 @@ namespace Code_of_the_rings
 
 			var stateCopy = (int[])this.State.Clone();
 			stateCopy[position] = desiredState;
-			return new BoardState(stateCopy);
+
+			return new BoardState(stateCopy, position);
+		}
+
+		public string MoveTo(BoardState other)
+		{
+			var move = string.Empty;
+
+			var distRight = Math.Abs(other.PlayerPosition - this.PlayerPosition);
+			var distLeft = this.PlayerPosition + 1 + (BoardSize - other.PlayerPosition);
+
+			if (distRight < distLeft)
+			{
+				move += Enumerable.Range(0, distRight).Select(x => ">").Aggregate((x, y) => x + y);
+			}
+			else
+			{
+				move += Enumerable.Range(0, distRight).Select(x => "<").Aggregate((x, y) => x + y);
+			}
+
+			var stateAtPlayersPosition = this.State[other.PlayerPosition];
+			var desiredStateAtPlayersPosition = other.State[other.PlayerPosition];
+
+			var distUp = Math.Abs(desiredStateAtPlayersPosition - stateAtPlayersPosition);
+			var distDown = stateAtPlayersPosition + 1 + (BoardSize - desiredStateAtPlayersPosition);
+
+			if (distUp < distDown)
+			{
+				move += Enumerable.Range(0, distUp).Select(x => "+").Aggregate((x, y) => x + y);
+			}
+			else
+			{
+				move += Enumerable.Range(0, distDown).Select(x => "-").Aggregate((x, y) => x + y);
+			}
+
+			move += ".";
+
+			return move;
 		}
 
 		public int GetDistanceTo(BoardState other)
 		{
 			return this.State
-				.Zip(other.State, (x, y) => Math.Min(y - x, x + 1 + (30 - y)))
-				.Sum();
+				.Zip(other.State, (x, y) => Math.Min(Math.Abs(y - x), x + 1 + (BoardSize - y)))
+				.Sum()
+				   //distance from player position to desired player position
+				   + Math.Min(Math.Abs(other.PlayerPosition - this.PlayerPosition), this.PlayerPosition + 1 + (BoardSize - other.PlayerPosition));
 		}
 	}
 
